59aaab628ebab1ffee366bab5c31f15530a8c696
tippitytapp <marc.tapp@gmail.com>
2020-05-15 11:35:34 -0400

    finished migration

--- data/migrations/20200515112052_createTables.js
+++ data/migrations/20200515112052_createTables.js
@@ -30,8 +30,39 @@ exports.up = function(knex) {
             .boolean('task_complete')
             .defaultTo(false);
     })
+    .createTable('resources', resources => {
+        resources.increments()
+        resources
+            .text('resource_name')
+            .notNullable();
+        resources
+            .text('resource_description')
+    })
+    .createTable('project_resources', project_resources => {
+        project_resources.increments()
+        project_resources
+            .integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT');
+        project_resources
+            .integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('resources')
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT')
+    })
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('project_resources')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('projects')
 };
