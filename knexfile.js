b1697b1867911a662431640d25f3d42c14891497
Marc Tapp <marc.tapp@gmail.com>
2020-05-16 14:40:06 -0400

    once gsin trying to deploy

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ const dbpassword = process.env.PASS;
 
 module.exports = {
 
-  development: {
+  development2: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
@@ -24,6 +24,16 @@ module.exports = {
   },
 
   production: {
+    client: 'pg',
+    connection: "postgres://mkrwmimwjxqbsy:f11a396cdf8732ffbeda052ff552ae75169234a4ca0904cd29d08b2c21a62e17@ec2-52-44-55-63.compute-1.amazonaws.com:5432/d6b0ali1u4rrpg",
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  development: {
     client: 'pg',
     connection: "postgres://mkrwmimwjxqbsy:f11a396cdf8732ffbeda052ff552ae75169234a4ca0904cd29d08b2c21a62e17@ec2-52-44-55-63.compute-1.amazonaws.com:5432/d6b0ali1u4rrpg",
     migrations: {
