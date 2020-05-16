c2b4ae8c8b815b76617ab43fc522fd248da5445a
Marc Tapp <marc.tapp@gmail.com>
2020-05-16 14:50:21 -0400

    trying to deploy

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,32 @@
 // Update with your config settings.
-require('dotenv').config();
-const dbpassword = process.env.PASS;
 
 module.exports = {
 
-  development2: {
+  development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
       filename: './data/projects.db3'
-    },
-    pool: {
-      afterCreate: (conn, done)=>{
-        conn.run('PRAGMA foreign_keys = ON', done)
-      }
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
     }
   },
-
   production: {
     client: 'pg',
-    connection: "postgres://mkrwmimwjxqbsy:f11a396cdf8732ffbeda052ff552ae75169234a4ca0904cd29d08b2c21a62e17@ec2-52-44-55-63.compute-1.amazonaws.com:5432/d6b0ali1u4rrpg",
-    migrations: {
-      directory: './data/migrations'
+    connection: {
+      host: 'ec2-52-44-55-63.compute-1.amazonaws.com',
+      database: 'd6b0ali1u4rrpg',
+      user:     'mkrwmimwjxqbsy',
+      password: 'f11a396cdf8732ffbeda052ff552ae75169234a4ca0904cd29d08b2c21a62e17'
     },
-    seeds: {
-      directory: './data/seeds'
-    }
-  },
-  development: {
-    client: 'pg',
-    connection: "postgres://mkrwmimwjxqbsy:f11a396cdf8732ffbeda052ff552ae75169234a4ca0904cd29d08b2c21a62e17@ec2-52-44-55-63.compute-1.amazonaws.com:5432/d6b0ali1u4rrpg",
-    migrations: {
-      directory: './data/migrations'
+    pool: {
+      min: 2,
+      max: 10
     },
-    seeds: {
-      directory: './data/seeds'
+    migrations: {
+      directory:'./data/migrations'
+    },
+    seeds:{
+      directory: "./data/seeds"
     }
-  },
+  }
+
 };
